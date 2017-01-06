7917389373139dade2fd35f64c8a1256f8fffc10
David Dotson <dldotson@asu.edu>
2017-01-06 11:21:38 -0700

    Added scikit-learn, scipy as dependencies.
    We are going to use some of scikit-learn's estimator base classes for
    our own estimators to get some automatic consistency in how they work,
    and we will need the scipy.integrate submodule for thermodynamic
    integration.

--- setup.py
+++ setup.py
@@ -25,5 +25,5 @@ setup(name='alchemlyb',
       package_dir={'': 'src'},
       license='BSD',
       long_description=open('README.rst').read(),
-      install_requires=['numpy', 'pandas', 'GromacsWrapper', 'pymbar']
+      install_requires=['numpy', 'pandas', 'GromacsWrapper', 'pymbar', 'scipy', 'scikit-learn']
       )
