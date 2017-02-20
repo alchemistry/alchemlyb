9c1016d0106107de06a087a67a594803e8bee7f9
David Dotson <dldotson@asu.edu>
2017-02-20 12:35:29 -0700

    Forgot to include updated conf.py for docs. :/

--- docs/conf.py
+++ docs/conf.py
@@ -161,4 +161,5 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/': None,
+                       'pandas': ('http://pandas.pydata.org/pandas-docs/stable/', None)}
