0ac647d7fffe5cfccfeff37e92e1e3ddaf14202c
David Dotson <dotsdl@gmail.com>
2017-02-28 14:19:51 -0700

    Another small test for gmx parsing.

--- src/alchemlyb/tests/parsing/test_gmx.py
+++ src/alchemlyb/tests/parsing/test_gmx.py
@@ -16,3 +16,20 @@ def test_dHdl():
             dHdl = extract_dHdl(filename, T=300)
 
             assert dHdl.index.names == ['time', 'fep-lambda']
+            assert dHdl.shape == (4001, 1)
+
+def test_u_nk():
+    """Test that u_nk has the correct form when extracted from files.
+
+    """
+    dataset = load_benzene()
+
+    for leg in dataset['data']:
+        for filename in dataset['data'][leg]:
+            u_nk = extract_u_nk(filename, T=300)
+
+            assert u_nk.index.names == ['time', 'fep-lambda']
+            if leg == 'Coulomb':
+                assert u_nk.shape == (4001, 5)
+            elif leg == 'VDW':
+                    u_nk.shape == (4001, 16)
