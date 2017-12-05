602381c271a01cdff298704153ab1594ad13b48e
trje3733 <trje3733@colorado.edu>
2017-12-05 14:16:37 -0700

    adding tests to parse expanded ensemble data

--- src/alchemlyb/tests/parsing/test_gmx.py
+++ src/alchemlyb/tests/parsing/test_gmx.py
@@ -4,6 +4,7 @@
 
 from alchemlyb.parsing.gmx import extract_dHdl, extract_u_nk
 from alchemtest.gmx import load_benzene
+from alchemtest.gmx import load_expanded_ensemble_case_1, load_expanded_ensemble_case_2, load_expanded_ensemble_case_3
 
 
 def test_dHdl():
@@ -34,3 +35,71 @@ def test_u_nk():
                 assert u_nk.shape == (4001, 5)
             elif leg == 'VDW':
                     u_nk.shape == (4001, 16)
+
+def test_u_nk_case1():
+    """Test that u_nk has the correct form when extracted from expanded ensemble files (case 1).
+
+    """
+    dataset = load_expanded_ensemble_case_1()
+
+    for leg in dataset['data']:
+        for filename in dataset['data'][leg]:
+            u_nk = extract_u_nk(filename, T=300)
+
+            assert u_nk.index.names == ['time', 'fep-lambda', 'coul-lambda', 'vdw-lambda', 'restraint-lambda']
+
+            assert u_nk.shape == (50001, 28)
+
+def test_dHdl_case1():
+    """Test that dHdl has the correct form when extracted from expanded ensemble files (case 1).
+
+    """
+    dataset = load_expanded_ensemble_case_1()
+
+    for leg in dataset['data']:
+        for filename in dataset['data'][leg]:
+            dHdl = extract_dHdl(filename, T=300)
+
+            assert dHdl.index.names == ['time', 'fep-lambda', 'coul-lambda', 'vdw-lambda', 'restraint-lambda']
+            assert dHdl.shape == (50001, 4)
+
+def test_u_nk_case2():
+    """Test that u_nk has the correct form when extracted from expanded ensemble files (case 2).
+
+    """
+    dataset = load_expanded_ensemble_case_2()
+
+    for leg in dataset['data']:
+        for filename in dataset['data'][leg]:
+            u_nk = extract_u_nk(filename, T=300)
+
+            assert u_nk.index.names == ['time', 'fep-lambda', 'coul-lambda', 'vdw-lambda', 'restraint-lambda']
+
+            assert u_nk.shape == (25001, 28)
+
+def test_u_nk_case3():
+    """Test that u_nk has the correct form when extracted from REX files (case 3).
+
+    """
+    dataset = load_expanded_ensemble_case_3()
+
+    for leg in dataset['data']:
+        for filename in dataset['data'][leg]:
+            u_nk = extract_u_nk(filename, T=300)
+
+            assert u_nk.index.names == ['time', 'fep-lambda', 'coul-lambda', 'vdw-lambda', 'restraint-lambda']
+
+            assert u_nk.shape == (2500, 28)
+
+def test_dHdl_case3():
+    """Test that dHdl has the correct form when extracted from REX files (case 3).
+
+    """
+    dataset = load_expanded_ensemble_case_3()
+
+    for leg in dataset['data']:
+        for filename in dataset['data'][leg]:
+            dHdl = extract_dHdl(filename, T=300)
+
+            assert dHdl.index.names == ['time', 'fep-lambda', 'coul-lambda', 'vdw-lambda', 'restraint-lambda']
+            assert dHdl.shape == (2500, 4)
