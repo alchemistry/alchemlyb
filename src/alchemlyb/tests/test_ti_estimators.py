f8ad5f046dc132d1a6404bf1475e6add24856e00
harlor <harlor@web.de>
2018-11-22 19:30:47 +0100

    Add TI uncertainty test

--- src/alchemlyb/tests/test_ti_estimators.py
+++ src/alchemlyb/tests/test_ti_estimators.py
@@ -71,18 +71,20 @@ def amber_simplesolvated_vdw_dHdl():
 
 class TIestimatorMixin:
 
-    @pytest.mark.parametrize('X_delta_f', ((gmx_benzene_coul_dHdl(), 3.089),
-                                           (gmx_benzene_vdw_dHdl(), -3.056),
-                                           (gmx_expanded_ensemble_case_1_dHdl(), 76.220),
-                                           (gmx_expanded_ensemble_case_2_dHdl(), 76.247),
-                                           (gmx_expanded_ensemble_case_3_dHdl(), 76.387),
-                                           (amber_simplesolvated_charge_dHdl(), -60.114),
-                                           (amber_simplesolvated_vdw_dHdl(), 3.824)))
+    @pytest.mark.parametrize('X_delta_f', ((gmx_benzene_coul_dHdl(), 3.089, 0.02157),
+                                           (gmx_benzene_vdw_dHdl(), -3.056, 0.04863),
+                                           (gmx_expanded_ensemble_case_1_dHdl(), 76.220, 0.15568),
+                                           (gmx_expanded_ensemble_case_2_dHdl(), 76.247, 0.15889),
+                                           (gmx_expanded_ensemble_case_3_dHdl(), 76.387, 0.12532),
+                                           (amber_simplesolvated_charge_dHdl(), -60.114, 0.08186),
+                                           (amber_simplesolvated_vdw_dHdl(), 3.824, 0.13254)))
     def test_get_delta_f(self, X_delta_f):
         est = self.cls().fit(X_delta_f[0])
         delta_f = est.delta_f_.iloc[0, -1]
+        d_delta_f = est.d_delta_f_.iloc[0, -1]
 
         assert X_delta_f[1] == pytest.approx(delta_f, rel=1e-3)
+        assert X_delta_f[2] == pytest.approx(d_delta_f, rel=1e-3)
 
 class TestTI(TIestimatorMixin):
     """Tests for TI.
