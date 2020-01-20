e8db1345feaae8bbcbb58e83fd20d6ebe8ea1531
David Dotson <dotsdl@gmail.com>
2020-01-20 16:46:34 -0700

    Added random_seed kwarg to bootstrap function

--- src/alchemlyb/preprocessing/bootstrapping.py
+++ src/alchemlyb/preprocessing/bootstrapping.py
@@ -2,22 +2,31 @@
 
 """
 import numpy as np
+import pandas as pd
 
-def bootstrap(df):
+
+def bootstrap(df, random_state=None):
     """Bootstrap sample a DataFrame to produce a new DataFrame.
 
     Values are sampled from `df` on a per-lambda-index basis.
+    Order of samples, or blocks of samples, are not preserved.
 
     Parameters
     ----------
     df : DataFrame
         DataFrame to bootstrap sample from.
+    random_state : int, optional
+        Integer between 0 and 2**32 -1 inclusive; fed to `numpy.random.seed`.
+        Running this function on the same data with a specific random seed will
+        produce the same result each time.
 
     Returns
     -------
     DataFrame
         DataFrame with the same shape as `df`, bootstrapped from the values of `df`.
     """
+    np.random.seed(random_state)
+
     df = df.copy()
     lambda_inds = [i for i in df.index.names if not i == 'time']
     
