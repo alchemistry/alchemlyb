c327215f9371846be7df23aa5f367350d5971079
Zhiyi Wu <zhiyi.wu@msdtc.ox.ac.uk>
2020-08-25 21:03:17 +0100

    fix test

--- src/alchemlyb/tests/test_visualisation.py
+++ src/alchemlyb/tests/test_visualisation.py
@@ -13,13 +13,13 @@ def test_plot_mbar_omatrix():
     mbar_coul = MBAR()
     mbar_coul.fit(u_nk_coul)
 
-    isinstance(plot_mbar_overlap_matrix(mbar_coul.overlap_maxtrix),
+    isinstance(plot_mbar_overlap_matrix(mbar_coul.overlap_matrix),
                matplotlib.axes.Axes)
-    isinstance(plot_mbar_overlap_matrix(mbar_coul.overlap_maxtrix, [1,]),
+    isinstance(plot_mbar_overlap_matrix(mbar_coul.overlap_matrix, [1,]),
                matplotlib.axes.Axes)
 
     # Bump up coverage
-    overlap_maxtrix = mbar_coul.overlap_maxtrix
+    overlap_maxtrix = mbar_coul.overlap_matrix
     overlap_maxtrix[0,0] = 0.0025
     overlap_maxtrix[-1, -1] = 0.9975
     isinstance(overlap_maxtrix,
