a2c2af29895caed9cf965ac0456c8d8d399e5aa8
Jérôme Hénin <heninj@gmail.com>
2021-05-07 19:54:22 +0200

    Initial support for IDWS output in NAMD
    that is, Interleaved Double-Wide Sampling, by Brannigan and Hénin.
    I had to arbitrarily match up fwd and backward data as if it came
    from the same configurations, otherwise BAR found zero overlap.
    However I don't think that's a fundamental requirement (fwd and bwd
    data could come from altogether different simulations).
    This seems like an unnecessary limitation of alchemlyb, or more likely,
    the underlying pymbar.

--- src/alchemlyb/parsing/namd.py
+++ src/alchemlyb/parsing/namd.py
@@ -42,55 +42,91 @@ def extract_u_nk(fep_file, T):
 
     # boolean flag to parse data after equil time
     parsing = False
+    lambda_idws = None
 
     # open and get data from fep file.
     with anyopen(fep_file, 'r') as f:
+        time = 0
         for line in f:
             l = line.strip().split()
 
+            # New window, get IDWS lambda if any
+            if l[0] == '#NEW':
+                if 'LAMBDA_IDWS' in l:
+                    lambda_idws = "{0:.2f}".format(float(l[10]))
+                else:
+                    lambda_idws = None
+
             # this line marks end of window; dump data into dataframe
             if '#Free' in l:
-
-                # convert last window's work and times values to np arrays
-                win_de_arr = beta * np.asarray(win_de)
-                win_ts_arr = np.asarray(win_ts)
 
                 # extract lambda values for finished window
                 # lambda1 = sampling lambda (row), lambda2 = evaluated lambda (col)
                 lambda1 = "{0:.2f}".format(float(l[7]))
                 lambda2 = "{0:.2f}".format(float(l[8]))
+                print(lambda1, lambda2, lambda_idws)
 
-                # create dataframe of times and work values
-                # this window's data goes in row LAMBDA1 and column LAMBDA2
-                tempDF = pd.DataFrame({
-                    'time': win_ts_arr,
-                    'fep-lambda': np.full(len(win_de_arr),lambda1),
-                    lambda1: 0,
-                    lambda2: win_de_arr})
+                # convert last window's work and times values to np arrays
+                win_de_arr = beta * np.asarray(win_de)
+                win_ts_arr = np.asarray(win_ts)
 
-                # join the new window's df to existing df
-                u_nk = pd.concat([u_nk, tempDF], sort=True)
+                if lambda_idws is not None:
+                    # Mimic classic DWS data
+                    # Arbitrarily match up fwd and bwd comparison energies on the same times
+                    # truncate extra samples from whichever array is longer
+                    win_de_back_arr = beta * np.asarray(win_de_back)
+                    n = min(len(win_de_back_arr), len(win_de_arr))
+
+                    tempDF_idws = pd.DataFrame({
+                        'time': range(n),
+                        'fep-lambda': np.full(n,lambda1),
+                        lambda1: 0,
+                        lambda2: win_de_arr[:n],
+                        lambda_idws: win_de_back_arr[:n]})
+
+                    # append to current dataframe
+                    u_nk = pd.concat([u_nk, tempDF_idws], sort=True)
+
+                else:
+                    # create dataframe of times and work values
+                    # this window's data goes in row LAMBDA1 and column LAMBDA2
+                    tempDF = pd.DataFrame({
+                        'time': win_ts_arr,
+                        'fep-lambda': np.full(len(win_de_arr),lambda1),
+                        lambda1: 0,
+                        lambda2: win_de_arr})
+
+                    # join the new window's df to existing df
+                    u_nk = pd.concat([u_nk, tempDF], sort=True)
 
                 # reset values for next window of fepout file
                 win_de = []
                 win_ts = []
+                win_de_back = []
+                win_ts_back = []
                 parsing = False
 
             # append work value from 'dE' column of fepout file
             if parsing:
-                win_de.append(float(l[6]))
-                win_ts.append(float(l[1]))
+                if l[0] == 'FepEnergy:':
+                    win_de.append(float(l[6]))
+                    win_ts.append(float(l[1]))
+                elif l[0] == 'FepE_back:':
+                    win_de_back.append(float(l[6]))
+                    win_ts_back.append(float(l[1]))
 
             # turn parsing on after line 'STARTING COLLECTION OF ENSEMBLE AVERAGE'
             if '#STARTING' in l:
                 parsing = True
 
-    # create last dataframe for fep-lambda at last LAMBDA2
-    tempDF = pd.DataFrame({
-        'time': win_ts_arr,
-        'fep-lambda': lambda2})
+    # TODO test if lambda2 is already in dataframe instead of test below
+    if (lambda2 == 1):
+        # create last dataframe for fep-lambda at last LAMBDA2
+        tempDF = pd.DataFrame({
+            'time': win_ts_arr,
+            'fep-lambda': lambda2})
 
-    u_nk = pd.concat([u_nk, tempDF], sort=True)
+        u_nk = pd.concat([u_nk, tempDF], sort=True)
 
     u_nk.set_index(['time','fep-lambda'], inplace=True)
 
