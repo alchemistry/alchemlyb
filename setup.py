40517fd64a5a800113082686b71e3f1cc5363927
Oliver Beckstein <orbeckst@gmail.com>
2021-06-10 13:24:00 -0700

    adopt NEP29
    - close #140
    - drop Python 3.6
    - require pandas >= 1.2
    - update setp.py metadata
    - update CHANGES
    - updated docs (add note to Development Model)

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(name='alchemlyb',
         'Operating System :: Microsoft :: Windows ',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -44,5 +43,5 @@ setup(name='alchemlyb',
       long_description=open('README.rst').read(),
       long_description_content_type='text/x-rst',
       tests_require = ['pytest', 'alchemtest'],
-      install_requires=['numpy', 'pandas>=1.0', 'pymbar>=3.0.5,<4', 'scipy', 'scikit-learn', 'matplotlib']
+      install_requires=['numpy', 'pandas>=1.2', 'pymbar>=3.0.5,<4', 'scipy', 'scikit-learn', 'matplotlib']
       )
