a92659780a7bc62653a785c2238d99385bf7897a
Zhiyi Wu <zhiyi.wu@bioch.ox.ac.uk>
2021-07-10 10:26:14 +0100

    update

--- src/alchemlyb/constants.py
+++
@@ -1,7 +0,0 @@
-'''Physical and mathematical constants and units.'''
-# Taken from scipy.constants since py2 doesn't support it
-#: Boltzmann's constant :math:`k_B` in kJ/(mol K); value from `NIST CODATA: k<https://physics.nist.gov/cgi-bin/cuu/Value?k>`_.
-Boltzmann_constant = 1.380649e-23
-#: Avogadro constant :math:`N_A` in 1/mol; value from `NIST CODATA: k<https://physics.nist.gov/cgi-bin/cuu/Value?na|search_for=avogadro>`_.
-Avogadro_constant = 6.02214076e+23
-kJ2kcal = 0.239006
