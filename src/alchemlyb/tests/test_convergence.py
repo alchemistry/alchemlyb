d28158253a83a942e31bbe0b55a0608fcd098bbd
Zhiyi Wu <zhiyi.wu@bioch.ox.ac.uk>
2021-10-07 20:16:06 +0100

    Update test_convergence.py

--- src/alchemlyb/tests/test_convergence.py
+++ src/alchemlyb/tests/test_convergence.py
@@ -13,7 +13,7 @@ def gmx_benzene():
 def test_convergence_ti(gmx_benzene):
     dHdl, u_nk = gmx_benzene
     convergence = forward_backward_convergence(dHdl, 'TI')
-    assert convergence.shape == (10, 4)
+    assert convergence.shape == (10, 5)
     assert convergence.iloc[0, 0] == pytest.approx(3.07, 0.01)
     assert convergence.iloc[0, 2] == pytest.approx(3.11, 0.01)
     assert convergence.iloc[-1, 0] == pytest.approx(3.09, 0.01)
@@ -22,7 +22,7 @@ def test_convergence_ti(gmx_benzene):
 def test_convergence_mbar(gmx_benzene):
     dHdl, u_nk = gmx_benzene
     convergence = forward_backward_convergence(u_nk, 'MBAR')
-    assert convergence.shape == (10, 4)
+    assert convergence.shape == (10, 5)
     assert convergence.iloc[0, 0] == pytest.approx(3.02, 0.01)
     assert convergence.iloc[0, 2] == pytest.approx(3.06, 0.01)
     assert convergence.iloc[-1, 0] == pytest.approx(3.05, 0.01)
