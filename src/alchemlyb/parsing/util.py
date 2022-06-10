5f6e46dc94cf0ac98198b52ad771f8b6386185cf
David Dotson <dotsdl@gmail.com>
2022-06-09 22:16:32 -0700

    Added checks for stream behavior in anyopen; dropped broken zip support
    We never had working zip archive support from the looks of it. This is
    because zipfiles are not just a compression scheme, they are an archive
    format that features compression.

--- src/alchemlyb/parsing/util.py
+++ src/alchemlyb/parsing/util.py
@@ -19,11 +19,11 @@ def gzip_open(filename, mode):
 def anyopen(datafile: Union[PathLike, IO], mode='r', compression=None):
     """Return a file stream for file or stream, even if compressed.
 
-    Supports files compressed with bzip2 (.bz2), gzip (.gz), and zip (.zip)
-    compression schemes. The appropriate extension must be present for
-    the function to properly handle the file.
+    Supports files compressed with bzip2 (.bz2) and gzip (.gz) compression
+    schemes. The appropriate extension must be present for the function to
+    properly handle the file.
 
-    If giving a stream for `datafile`, then you must specify `compression` if 
+    If giving a stream for `datafile`, then you must specify `compression` if
     the stream is compressed.
 
     Parameters
@@ -35,7 +35,7 @@ def anyopen(datafile: Union[PathLike, IO], mode='r', compression=None):
         Mode for stream; usually 'r' or 'w'.
     compression : str
         Use to specify compression.
-        Must be one of 'bz2', 'gz', 'zip'.
+        Must be one of 'bz2', 'gz'.
 
     Returns
     -------
@@ -45,22 +45,21 @@ def anyopen(datafile: Union[PathLike, IO], mode='r', compression=None):
     """
     # opener for each type of file
     extensions = {'.bz2': bz2_open,
-                  '.gz': gzip_open,
-                  '.zip': zipfile.ZipFile}
+                  '.gz': gzip_open}
 
     # compression selections available
     compressions = {'bz2': bz2_open,
-                    'gz': gzip_open,
-                    'zip': zipfile.ZipFile}
+                    'gz': gzip_open}
 
     # if `datafile` is a stream
-    if hasattr(datafile, 'read'):
+    if ((hasattr(datafile, 'read') and any((i in mode for i in ('r',)))) or 
+       (hasattr(datafile, 'write') and any((i in mode for i in ('w', 'a', 'x'))))):
         # if no compression specified, just pass the stream through
         if compression is None:
             return datafile
         elif compression in compressions:
-            decompressor = compressions[compression]
-            return decompressor(datafile, mode=mode)
+            compressor = compressions[compression]
+            return compressor(datafile, mode=mode)
 
     # otherwise, treat as a file
     ext = os.path.splitext(datafile)[1]
--- src/alchemlyb/tests/parsing/test_util.py
+++ src/alchemlyb/tests/parsing/test_util.py
@@ -1,3 +1,4 @@
+import io
 import pytest
 
 from alchemtest.gmx import load_expanded_ensemble_case_1
@@ -53,3 +54,43 @@ def test_gzip_stream_wrong_no_compression():
             with open(filename, 'rb') as f:
                 with anyopen(f, mode='r') as f_uc:
                     assert type(f_uc.readline()) is bytes
+
+@pytest.mark.parametrize('compression', ['bz2', 'gz'])
+def test_file_roundtrip(compression, tmp_path):
+    """Test that roundtripping write/read to a file works with `anyopen`
+
+    """
+
+    data = "my momma told me to pick the very best one and you are not it"
+
+    filepath = tmp_path / f'testfile.txt.{compression}'
+    with anyopen(filepath, mode='w') as f:
+        f.write(data)
+
+    with anyopen(filepath, 'r') as f:
+        data_out = f.read()
+
+    assert data_out == data
+
+@pytest.mark.parametrize('compression', ['bz2', 'gz'])
+def test_file_roundtrip(compression):
+    """Test that roundtripping write/read to a file works with `anyopen`
+
+    """
+
+    data = "my momma told me to pick the very best one and you are not it"
+
+    with io.BytesIO() as stream:
+
+        # write to stream
+        with anyopen(stream, mode='w', compression=compression) as f:
+            f.write(data)
+
+        # start at the beginning
+        stream.seek(0)
+
+        # read from stream
+        with anyopen(stream, 'r', compression=compression) as f:
+            data_out = f.read()
+
+        assert data_out == data
