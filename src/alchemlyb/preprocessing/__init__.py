eda09e07e4c9e4f43319d7ec87c52aa51b1d9ea5
Zhiyi Wu <zwu@exscientia.co.uk>
2022-08-18 11:47:09 +0100

    fix init

--- src/alchemlyb/preprocessing/__init__.py
+++ src/alchemlyb/preprocessing/__init__.py
@@ -3,7 +3,7 @@ The :mod:`alchemlyb.preprocessing` module includes methods for subsampling and
 preparing data for estimators.
 """
 
-from .subsampling import slicing, dhdl2series, u_nk2series
+from .subsampling import slicing, dhdl2series, u_nk2series, decorrelate_dhdl, decorrelate_u_nk
 from .subsampling import statistical_inefficiency
 from .subsampling import equilibrium_detection
 
@@ -11,6 +11,8 @@ __all__ = [
     'slicing',
     'statistical_inefficiency',
     'equilibrium_detection',
+    'decorrelate_dhdl',
+    'decorrelate_u_nk',
     'dhdl2series',
     'u_nk2series'
 ]
