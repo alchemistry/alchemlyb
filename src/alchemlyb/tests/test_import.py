99f3f698ec38790f66b0185a70e0046ee061e52b
DrDomenicoMarson <domenico87@gmail.com>
2022-09-25 23:08:17 +0200

    removed a redundant try/except in the test

--- src/alchemlyb/tests/test_import.py
+++ src/alchemlyb/tests/test_import.py
@@ -1,7 +1,4 @@
 import alchemlyb
 
 def test_name():
-    try:
-        assert alchemlyb.__name__ == 'alchemlyb'
-    except Exception as e:
-        raise e
+    assert alchemlyb.__name__ == 'alchemlyb'
