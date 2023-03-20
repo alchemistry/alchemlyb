5263ed4e0049a430b3586861a11b49247aed6104
Zhiyi Wu <zwu@exscientia.ai>
2023-03-20 17:07:49 +0000

    Update subsampling.py

--- src/alchemlyb/preprocessing/subsampling.py
+++ src/alchemlyb/preprocessing/subsampling.py
@@ -153,6 +153,9 @@ def u_nk2series(df, method="dE"):
 
 
     .. versionadded:: 1.0.0
+    .. versionchanged:: 2.0.1
+       The `dE` method now computed the difference between the current lambda
+       and the next lambda (previous lambda for the last window).
 
     """
 
