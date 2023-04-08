268402663918b01193f414e63353d216a9a857c6
hl2500 <49323647+hl2500@users.noreply.github.com>
2023-04-08 17:26:12 -0400

    Add files via upload
    separate TI estimator with gaussian quadrature

--- src/alchemlyb/estimators/ti_gaussian_quadrature.py
+++ src/alchemlyb/estimators/ti_gaussian_quadrature.py
@@ -5,8 +5,8 @@ from sklearn.base import BaseEstimator
 from .base import _EstimatorMixOut
 
 
-class TI(BaseEstimator, _EstimatorMixOut):
-    """Thermodynamic integration (TI).
+class TI_gq(BaseEstimator, _EstimatorMixOut):
+    """Thermodynamic integration (TI) with gaussian quadrature estimation.
 
     Parameters
     ----------
@@ -25,13 +25,13 @@ class TI(BaseEstimator, _EstimatorMixOut):
         dimensionless free energy differences.
 
     states_ : list
-        Lambda states for which free energy differences were obtained.
+        Lambda states for which free energy estimation were obtained.
 
     dhdl : DataFrame
         The estimated dhdl of each state.
 
 
-    .. versionchanged:: 1.0.0
+    .. versionchanged:: 2.0.0
        `delta_f_`, `d_delta_f_`, `states_` are view of the original object.
 
     """
@@ -62,61 +62,58 @@ class TI(BaseEstimator, _EstimatorMixOut):
         means = dHdl.groupby(level=dHdl.index.names[1:]).mean()
         variances = np.square(dHdl.groupby(level=dHdl.index.names[1:]).sem())
 
-        # get the lambda names
-        l_types = dHdl.index.names[1:]
+        # extract the lambda values used in the simulations
+        lambdas = means.reset_index()[means.index.names[:]].iloc[:].values.sum(axis=1)
+        num_lambdas = len(lambdas)
+        # suggested lambda values corresponding weights 
+        special_points = {1: [[0.5], [1.0]], 2: [[0.21132, 0.78867], [0.5, 0.5]],
+                          3: [[0.1127, 0.5, 0.88729], [0.27777, 0.44444, 0.27777]],
+                          5: [[0.04691, 0.23076, 0.5, 0.76923, 0.95308], [0.11846, 0.23931, 0.28444, 0.23931, 0.11846]], 
+                          7: [[0.02544, 0.12923, 0.29707, 0.5, 0.70292, 0.87076, 0.97455], 
+                              [0.06474, 0.13985, 0.19091, 0.20897, 0.19091, 0.13985, 0.06474]],
+                          9: [[0.01592, 0.08198, 0.19331, 0.33787, 0.5, 0.66213, 0.80669, 0.91802, 0.98408],
+                              [0.04064, 0.09032, 0.13031, 0.15617, 0.16512, 0.15617, 0.13031, 0.09032, 0.04064]],
+                         12: [[0.00922, 0.04794, 0.11505, 0.20634, 0.31608, 0.43738, 0.56262, 0.68392, 0.79366, 0.88495, 0.95206, 0.99078],
+                              [0.02359, 0.05347, 0.08004, 0.10158, 0.11675, 0.12457, 0.12457, 0.11675, 0.10158, 0.08004, 0.05347, 0.02359]]
+                         }
+        if (num_lambdas not in special_points) or (not np.allclose(lambdas, special_points[num_lambdas][0], rtol=0.1)):
+            raise ValueError('The lambda values for gaussian quadrature are not supported, please use trapezoid rule instead.') 
+        
+        weights = special_points[num_lambdas][1]
+        mean_values = means.values.sum(axis=1)
+        variance_values = variances.values.sum(axis=1)
 
-        # obtain vector of delta lambdas between each state
-        # Fix issue #148, where for pandas == 1.3.0
-        # dl = means.reset_index()[list(means.index.names[:])].diff().iloc[1:].values
-        dl = means.reset_index()[means.index.names[:]].diff().iloc[1:].values
-
-        # apply trapezoid rule to obtain DF between each adjacent state
-        deltas = (dl * (means.iloc[:-1].values + means.iloc[1:].values) / 2).sum(axis=1)
-
+        # apply gaussian quadrature multiplication at each lambda state 
+        deltas = weights * mean_values
+        d_deltas_squared = np.square(weights) * variance_values
         # build matrix of deltas between each state
-        adelta = np.zeros((len(deltas) + 1, len(deltas) + 1))
+        adelta = np.zeros((len(deltas), len(deltas)))
         ad_delta = np.zeros_like(adelta)
 
         for j in range(len(deltas)):
             out = []
             dout = []
             for i in range(len(deltas) - j):
+                # Append cumulative free energy value from state i to i+j
                 out.append(deltas[i] + deltas[i + 1 : i + j + 1].sum())
+                # Append cumulative squared deviation of free energy from state i to i+j
+                dout.append(d_deltas_squared[i] + d_deltas_squared[i + 1 : i + j + 1].sum())
+            
+            adelta += np.diagflat(np.array(out), k=j)
+            ad_delta += np.diagflat(np.array(dout), k=j)
 
-                # Define additional zero lambda
-                a = [0.0] * len(l_types)
-
-                # Define dl series' with additional zero lambda on the left and right
-                dll = np.insert(dl[i : i + j + 1], 0, [a], axis=0)
-                dlr = np.append(dl[i : i + j + 1], [a], axis=0)
-
-                # Get a series of the form: x1, x1 + x2, ..., x(n-1) + x(n), x(n)
-                dllr = dll + dlr
-
-                # Append deviation of free energy difference between state i and i+j+1
-                dout.append(
-                    (dllr**2 * variances.iloc[i : i + j + 2].values / 4)
-                    .sum(axis=1)
-                    .sum()
-                )
-            adelta += np.diagflat(np.array(out), k=j + 1)
-            ad_delta += np.diagflat(np.array(dout), k=j + 1)
-
-        # yield standard delta_f_ free energies between each state
+        # yield standard delta_f_ cumulative free energies from one state to another
         self._delta_f_ = pd.DataFrame(
-            adelta - adelta.T, columns=means.index.values, index=means.index.values
+            adelta, columns=means.index.values, index=means.index.values
         )
         self.dhdl = means
-
         # yield standard deviation d_delta_f_ between each state
         self._d_delta_f_ = pd.DataFrame(
-            np.sqrt(ad_delta + ad_delta.T),
+            np.sqrt(ad_delta),
             columns=variances.index.values,
             index=variances.index.values,
         )
-
         self._states_ = means.index.values.tolist()
-
         self._delta_f_.attrs = dHdl.attrs
         self._d_delta_f_.attrs = dHdl.attrs
         self.dhdl.attrs = dHdl.attrs
