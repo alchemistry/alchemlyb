6f6edd06c610de71ebac24d24cc06b56e10f4569
William (Zhiyi) Wu <zwu@exscientia.co.uk>
2023-04-17 10:49:02 +0100

    update

--- src/alchemlyb/tests/test_workflow_ABFE.py
+++ src/alchemlyb/tests/test_workflow_ABFE.py
@@ -52,13 +52,14 @@ class TestInit:
                 T=310,
             )
     def test_wildcard_in_dir(self):
-        with pytest.warns(match="A real directory is expected in `dir`="):
-            ABFE(
-                dir="/*/",
-                prefix="dhdl",
-                suffix="xvg",
-                T=310,
-            )
+        with pytest.raises(ValueError):
+            with pytest.warns(match="A real directory is expected in `dir`="):
+                ABFE(
+                    dir="/*/",
+                    prefix="dhdl",
+                    suffix="xvg",
+                    T=310,
+                )
 
 
 class TestRun:
