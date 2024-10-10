2213db703b161a1bda012c6827b17e1b86a682b4
evidencebp <idanamit@gmail.com>
2024-10-10 12:59:24 +0300

    src\alchemlyb\parsing\amber.py  line-too-long
    Made two readable lines shorter

--- src/alchemlyb/parsing/amber.py
+++ src/alchemlyb/parsing/amber.py
@@ -334,7 +334,8 @@ def extract(outfile, T):
                 if nstep != old_nstep and dvdl is not None and nstep is not None:
                     if finished:
                         raise ValueError(
-                            "TI Energy detected after the TIMINGS section. Did you concatenate the output file?"
+                            "TI Energy detected after the TIMINGS section."
+                            + " Did you concatenate the output file?"
                         )
                     file_datum.gradients.append(dvdl)
                     nensec += 1
@@ -342,7 +343,8 @@ def extract(outfile, T):
             elif line.startswith("MBAR Energy analysis") and file_datum.have_mbar:
                 if finished:
                     raise ValueError(
-                        "MBAR Energy detected after the TIMINGS section. Did you concatenate the output file?"
+                        "MBAR Energy detected after the TIMINGS section."
+                        + " Did you concatenate the output file?"
                     )
                 mbar = secp.extract_section(
                     "^MBAR", "^ ---", file_datum.mbar_lambdas, extra=line
